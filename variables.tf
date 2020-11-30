3f1d8f7c70bb5dbc954a18b468ae9779fe58bf88
Caio Henrique <caio@ganex.com.br>
2020-11-30 18:17:28 -0300

    Add initial files

---
+++ variables.tf
@@ -0,0 +1,171 @@
+variable "sns_topic_arn" {
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on alerts"
+  type        = list(string)
+}
+
+variable "alarm_name_prefix" {
+  description = "Alarm name prefix"
+  type        = string
+  default     = ""
+}
+
+variable "cache_cluster_id" {
+  description = "The instance ID of the Cache instances that you want to monitor."
+  type        = list(string)
+}
+
+variable "cpu_utilization_threshold" {
+  description = "The maximum percentage of CPU utilization."
+  type        = number
+  default     = 90
+}
+
+variable "engine_cpu_utilization_threshold" {
+  description = "The maximum percentage of Engine CPU utilization."
+  type        = number
+  default     = 90
+}
+
+variable "currconnections_threshold" {
+  description = "The maximum number of connections."
+  type        = number
+  default     = 4000
+}
+
+variable "memory_utilization_threshold" {
+  description = "The maximum percentage of Memory utilization."
+  type        = number
+  default     = 90
+}
+
+variable "swap_usage_threshold" {
+  description = "The maximum amount of swap space used on the DB instance in Byte."
+  type        = number
+  default     = 256000000
+}
+
+variable "evictions_threshold" {
+  description = "The maximum number of evictions."
+  type        = number
+  default     = 100
+}
+
+variable "cpu_utilization_too_high-alarm" {
+  description = "Enable Alarm to metric: cpu_utilization_too_high"
+  default     = "true"
+}
+
+variable "cpu_utilization_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "cpu_utilization_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "cpu_utilization_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "300"
+}
+
+variable "engine_cpu_utilization_too_high-alarm" {
+  description = "Enable Alarm to metric: engine_cpu_utilization_too_high"
+  default     = "true"
+}
+
+variable "engine_cpu_utilization_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "engine_cpu_utilization_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "engine_cpu_utilization_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "300"
+}
+
+variable "currconnections_too_high-alarm" {
+  description = "Enable Alarm to metric: cpu_utilization_too_high"
+  default     = "true"
+}
+
+variable "currconnections_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "currconnections_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "currconnections_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "300"
+}
+
+variable "memory_utilization_too_high-alarm" {
+  description = "Enable Alarm to metric: memory_utilization_too_high"
+  default     = "true"
+}
+
+variable "memory_utilization_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "memory_utilization_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "memory_utilization_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "300"
+}
+
+variable "swap_usage_too_high-alarm" {
+  description = "Enable Alaarm to metric: swap_usage_too_high"
+  default     = "true"
+}
+
+variable "swap_usage_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "swap_usage_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "swap_usage_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "600"
+}
+
+variable "evictions_too_high-alarm" {
+  description = "Enable Alaarm to metric: evictions_too_high"
+  default     = "true"
+}
+
+variable "evictions_too_high-comparison_operator" {
+  description = "Comparison_operator to alarm"
+  default     = "GreaterThanThreshold"
+}
+
+variable "evictions_too_high-datapoint" {
+  description = "Datapoint check to alarm"
+  default     = "1"
+}
+
+variable "evictions_too_high-period" {
+  description = "Period check to alarm (in seconds)"
+  default     = "600"
+}
